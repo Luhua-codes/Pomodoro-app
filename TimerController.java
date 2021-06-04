fb79fa6bd34841eb5ffbe48f562f04ce3bc7e200
unknown <kierahutton1@gmail.com>
2021-06-03 20:38:16 -0600

    added cycle repetitions to cycle1 method and wrote a suggestion for a slightly different way of doing the cycles

--- TimerController.java
+++ TimerController.java
@@ -5,12 +5,36 @@ public final class TimerController {
 		throw new UnsupportedOperationException("Cannot create object");
 	}
 	
-	public static void cycle1() {//25 and 5 four times
-		Timer timer25 = new Timer(25);//change to minutes
-		Timer timer5 = new Timer (5);
-		for (int x = 0; x < 4; x++) {
-			timer25.runTimer();
-			timer5.runTimer();
+	public static void cycle1(int cycleReps) {//25 and 5 four times
+		Timer timer25 = new Timer(25, 0);
+		Timer timer5 = new Timer(5, 0);
+		Timer timer15 = new Timer(15, 0);
+		for (int cycle = 0; cycle < cycleReps; cycle++) {
+			for (int x = 0; x < 4; x++) {
+				timer25.runTimer();
+				timer5.runTimer();
+			}
+			timer15.runTimer();
+		}
+	}
+	
+	public static void runCycle(int cycleNumber, int cycleReps) {//we could have a bunch of identical methods with different times but that seems redundant; here's my alternative idea
+		int studyTime, shortBreak, shortBreakReps, longBreak;
+		//if (cycleNumber == 1) { //commented out because we don't have the elses yet and eclipse gives an error without them
+			studyTime = 25;
+			shortBreak = 5;
+			shortBreakReps = 4;
+			longBreak = 15;
+		//}
+		Timer studyTimer = new Timer(studyTime, 0);
+		Timer sBreakTimer = new Timer(shortBreak, 0);
+		Timer lBreakTimer = new Timer (longBreak, 0);
+		for (int cycle = 0; cycle < cycleReps; cycle++) {
+			for (int x = 0; x < shortBreakReps; x++) {
+				studyTimer.runTimer();
+				sBreakTimer.runTimer();
+			}
+			lBreakTimer.runTimer();
 		}
 	}
 	
