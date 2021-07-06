39dff387a6779784daac3527334bbf422e2ea513
unknown <kierahutton1@gmail.com>
2021-07-05 19:59:52 -0600

    implemented the cycle class and stored cycles in an array list, and wrote user cycle creation

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,33 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 class Main {
+	static Scanner input = new Scanner(System.in);
 
-    public static int timerSelection() {
-        Scanner input = new Scanner(System.in);
+    public static int timerSelection() {//maybe call runCycle method in here
         System.out.print("Enter cycle number: ");
         int choice = input.nextInt();
-        input.close();
         return choice;
+    }
+    public static void addCycle() {
+    	System.out.println("Enter length of study time");
+    	int studyTime = input.nextInt();
+    	System.out.println("Enter length of short break");
+    	int shortBreak = input.nextInt();
+    	System.out.println("Enter number of short breaks");
+    	int shortBreakReps = input.nextInt();
+    	System.out.println("Enter lenghth of long break");
+    	int longBreak = input.nextInt();
+    	TimerController.addCycle(studyTime, shortBreak, shortBreakReps, longBreak); //add possibility of different units
+    	System.out.println(TimerController.getCycles());
     }
 
     public static void main(String[] args) {
-        TimerController.runCycle(timerSelection(), 2);
+        System.out.println("Enter 1 to chose a cycle or 2 to create a cycle");
+        int choice = input.nextInt();
+        if (choice == 1)
+            TimerController.runCycle(timerSelection(), 2);
+        else
+        	addCycle();
+        input.close();
     }
 }
--- src/TimerController.java
+++ src/TimerController.java
@@ -1,43 +1,46 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public final class TimerController {
 
     private TimerController() {
         throw new UnsupportedOperationException("Cannot create object");
     }
+    
+    private static ArrayList<Cycle> cycles = new ArrayList<Cycle>(
+    		Arrays.asList(new Cycle(5,1,4,2),//the List.of method would also work
+    		new Cycle(25,5,4,15),
+    		new Cycle(50,10,4,30),
+    		new Cycle(90,15,4,45)));
 
-    public static void runCycle(int cycleNumber, int cycleReps) {//we could have a bunch of identical methods with different times but that seems redundant; here's my alternative idea
-        int studyTime = 25, shortBreak = 5, shortBreakReps = 4, longBreak = 15; //default cycle
-        if (cycleNumber == 2) {
-            studyTime = 50;
-            shortBreak = 10;
-            shortBreakReps = 4;
-            longBreak = 30;
-        } else if (cycleNumber == 3) {
-            studyTime = 90;
-            shortBreak = 15;
-            shortBreakReps = 4;
-            longBreak = 45;
-        } else if (cycleNumber == 0) {
-            studyTime = 5;
-            shortBreak = 1;
-            shortBreakReps = 4;
-            longBreak = 2;
-        }
+    public static void addCycle(int studyTime, int shortBreak, int shortBreakReps, int longBreak) {
+    	cycles.add(new Cycle(studyTime, shortBreak, shortBreakReps, longBreak));
+    }
+    public static ArrayList getCycles() {
+    	return cycles;
+    }
+
+	public static void runCycle(int cycleNumber, int cycleReps) {//we could have a bunch of identical methods with different times but that seems redundant; here's my alternative idea
 //		Timer studyTimer = new Timer(studyTime, 0, "Focus time");
 //		Timer sBreakTimer = new Timer(shortBreak, 0, "Short break");
 //		Timer lBreakTimer = new Timer (longBreak, 0, "Long Break");
 
         //testing code (seconds)
-        Timer studyTimer = new Timer(studyTime, "Focus time");
-        Timer sBreakTimer = new Timer(shortBreak, "Short break");
-        Timer lBreakTimer = new Timer(longBreak, "Long Break");
+        Timer studyTimer = new Timer(cycles.get(cycleNumber).getStudyTime(), "Focus time");
+        Timer sBreakTimer = new Timer(cycles.get(cycleNumber).getShortBreak(), "Short break");
+        Timer lBreakTimer = new Timer(cycles.get(cycleNumber).getLongBreak(), "Long Break");
 
         for (int cycle = 0; cycle < cycleReps; cycle++) {
-            for (int x = 0; x < shortBreakReps; x++) {
+            for (int x = 0; x < cycles.get(cycleNumber).getShortBreakReps(); x++) {
                 studyTimer.runTimer();
-                if (x < shortBreakReps - 1)
+                if (x < cycles.get(cycleNumber).getShortBreakReps() - 1)
                     sBreakTimer.runTimer();
+            }
+            if (cycle == cycleReps - 1) {
+            	System.out.println("Pomodoro complete.");
+            	break;
             }
             System.out.println("Study cycle finished. Long break starting | " + (cycleReps - cycle - 1) + " cycles remaining");
             lBreakTimer.runTimer();
