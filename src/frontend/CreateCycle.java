068edda6a3a00bb73a659f0f006e9a61482be1c9
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-01-19 17:10:47 -0500

    Cleaned up whitespace in CreateCycle, made frame and panel final

--- src/frontend/CreateCycle.java
+++ src/frontend/CreateCycle.java
@@ -5,8 +5,8 @@ import java.awt.*;
 import javax.swing.*;
 
 public class CreateCycle {
-	private JFrame createCycleFrame = new JFrame("View Existing Cycles");
-	private JPanel createCyclePanel = new JPanel();
+	private final JFrame createCycleFrame = new JFrame("View Existing Cycles");
+	private final JPanel createCyclePanel = new JPanel();
 	
 	public void show() {
 		frameSetup();
@@ -28,16 +28,11 @@ public class CreateCycle {
 		createCyclePanel.add(appTitle);
 		createCyclePanel.add(Box.createRigidArea(new Dimension(0, 10)));
 		
-		//fix text field positioning, add action listener for button, and add error checking
+		//**fix text field positioning, add action listener for button, and add error checking**
 		TextField[] textFields = {new TextField("Name"), new TextField("Study time"), new TextField("Short break"), new TextField("Short break repetitions"), new TextField("Long break")};
-		
 		Button submit = new Button("Submit");
 		
-		for (TextField t: textFields) {
-			createCyclePanel.add(t);
-		}
-		
+		for (TextField t: textFields) {createCyclePanel.add(t);}
 		createCyclePanel.add(submit);
-
 	}
 }
