bf29b144b7b66495a8d72db46d55d628455773c6
unknown <kierahutton1@gmail.com>
2022-02-02 12:23:30 -0500

    fixed display of CreateCycle using GridBagLayout.

--- src/frontend/CreateCycle.java
+++ src/frontend/CreateCycle.java
@@ -7,9 +7,12 @@ import javax.swing.*;
 public class CreateCycle {
 	private JFrame createCycleFrame = new JFrame("View Existing Cycles");
 	private JPanel createCyclePanel = new JPanel();
+	private GridBagConstraints gbc = new GridBagConstraints();
+	private GridBagLayout layout = new GridBagLayout();
 	
 	public void show() {
 		frameSetup();
+		panelSetup();
 		screenElements();
 
 		createCycleFrame.getContentPane().add(createCyclePanel);
@@ -19,25 +22,47 @@ public class CreateCycle {
 	
 	private void frameSetup() {
 		createCycleFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //https://chortle.ccsu.edu/java5/notes/chap56/ch56_9.html
-		int frameWidth = 400, frameHeight = 400;
+		int frameWidth = 600, frameHeight = 300;
 		createCycleFrame.setSize(frameWidth, frameHeight);
+		createCycleFrame.setResizable(false);
+	}
+	
+	private void panelSetup() {
+		createCyclePanel.setLayout(layout);
 	}
 	
 	private void screenElements() {
-		JLabel appTitle = new JLabel("Pomodoro app");
-		createCyclePanel.add(appTitle);
-		createCyclePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		int yposition = 0;
 		
-		//fix text field positioning, add action listener for button, and add error checking
+		JLabel appTitle = new JLabel("Pomodoro app");
+		gbc.gridx = 0;
+		gbc.gridy = yposition;
+		gbc.weighty = 0.2;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		createCyclePanel.add(appTitle);
+		yposition++;
+		//createCyclePanel.add(Box.createRigidArea(new Dimension(0, 10)));
+		
+		//add action listener for button, and add error checking
 		TextField[] textFields = {new TextField("Name"), new TextField("Study time"), new TextField("Short break"), new TextField("Short break repetitions"), new TextField("Long break")};
 		
 		Button submit = new Button("Submit");
 		
-		for (TextField t: textFields) {
-			createCyclePanel.add(t);
+		for (TextField t: textFields) {			
+			gbc.gridx = 0;
+			gbc.gridy = yposition;
+			gbc.weighty = 0.2;
+			gbc.anchor = GridBagConstraints.LINE_START;
+			createCyclePanel.add(t, gbc);
+			yposition++;
 		}
 		
-		createCyclePanel.add(submit);
+		gbc.gridx = 0;
+		gbc.gridy = yposition;
+		gbc.weighty = 0.2;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		createCyclePanel.add(submit, gbc);
+		yposition++;
 
 	}
 }
